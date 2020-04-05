1157d411d98a33a5db55d7f11956954824ae1005
Adam Priest <sepone229@gmail.com>
2020-04-05 22:55:48 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,12 +1,10 @@
 class Triangle
   attr_accessor :side1, :side2, :side3
   @sides = []
-
   def initialize(side1, side2, side3)
     @side1, @side2, @side3 = side1, side2, side3
     @sides = [side1, side2, side3]
   end
-
   def kind
     if valid? == false
       raise TriangleError
@@ -18,26 +16,21 @@ class Triangle
       :isosceles
     end
   end
-
   def equilateral?
     (@side1 == @side2) && (@side1 == @side3)
   end
-
   def scalene?
     (@side1 != @side2) && (@side1!= @side3) && (@side2 != @side3)
   end
-
   def isosceles?
     (@side2 == @side3) || (@side1 == @side2) || (@side1 = @side3)
   end
-
   def valid?
     if (@side1 + @side2 > @side3) && (@side2 + @side3 > @side1) && (@side1 + @side3 > @side2)
       return true
     else
       raise TriangleError
     end
-    
   end
   class TriangleError < StandardError
     def message
