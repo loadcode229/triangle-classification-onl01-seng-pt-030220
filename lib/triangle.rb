93bed545f4f1e3e37d59230739c0fb57aa43be76
Adam Priest <sepone229@gmail.com>
2020-04-05 23:17:03 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -23,7 +23,7 @@ class Triangle
     (@side1 != @side2) && (@side1!= @side3) && (@side2 != @side3)
   end
   def isosceles?
-    (@side2 == @side3) || (@side1 == @side2) || (@side1 = @side3)
+    (@side2 == @side3) || (@side1 == @side2) || (@side1 == @side3)
   end
   def valid?
     if (@side1 + @side2 > @side3) && (@side2 + @side3 > @side1) && (@side1 + @side3 > @side2)
